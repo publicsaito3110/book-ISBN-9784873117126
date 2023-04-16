e033649d5fa47915df2d87d63605249dfabd4606
publicsaito3110 <public.business.saito3110@gmail.com>
2023-04-16 20:13:10 +0900

    update: practiced

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,47 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+  // Answer
+  
+  // Set R2 = 0
+  @R2
+  M = 0
+  
+  // Set i = 0
+  @i
+  M = 1
+  
+  (LOOP)
+    
+    // Call i
+    @i
+    D = M
+    
+    // Call R1 And Set R1 = i - R1
+    @R1
+    D = D - M
+    
+    // Loop End Condition 0 < R1
+    @END
+    D;JGT
+    
+    // Call R0
+    @R0
+    D = M
+    
+    // Call R2 And Set R2 = R2 + R0
+    @R2
+    M = M + D
+  
+    // Call i And Set i++
+    @i
+    M = M + 1
+    
+    // Back Loop's Start
+    @LOOP
+    0;JMP
+  (END)
+    
+    // Back Loop's Start
+    @END
+    0;JMP
