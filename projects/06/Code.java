44dd22ba3a04668020cd909ff0da1010790a7397
publicsaito3110 <public.business.saito3110@gmail.com>
2023-05-06 21:25:04 +0900

    update: proceed

--- projects/06/Code.java
+++ projects/06/Code.java
@@ -1,131 +1,125 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class Code {
 
-	//---------------------
-	// Const dest command
-	//---------------------
+	private Map<String, String> destMap = new HashMap<>();
 
-	private static final String SYMBOL_DEST_NULL = "NULL";
+	private Map<String, String> compMap = new HashMap<>();
 
-	private static final String SYMBOL_DEST_M = "M";
+	private Map<String, String> jumpMap = new HashMap<>();
 
-	private static final String SYMBOL_DEST_D = "D";
 
-	private static final String SYMBOL_DEST_MD = "MD";
+	/**
+	 * Constractor
+	 *
+	 * Road defoult C-instruction's binary of 'dest', 'comp' and 'jump'
+	 *
+	 * @param void
+	 * @return SymbolTable
+	 */
+	public Code() {
 
-	private static final String SYMBOL_DEST_A = "A";
+		// Set dest binary
+		destMap.put(null, "000");
+		destMap.put("0", "000");
+		destMap.put("M", "001");
+		destMap.put("D", "010");
+		destMap.put("MD", "011");
+		destMap.put("A", "100");
+		destMap.put("AM", "101");
+		destMap.put("AD", "110");
+		destMap.put("AMD", "111");
 
-	private static final String SYMBOL_DEST_AM = "AM";
+		// Set comp binary
+		compMap.put("A>>", "010000000");
+		compMap.put("D>>", "010010000");
+		compMap.put("A<<", "010100000");
+		compMap.put("D<<", "010110000");
+		compMap.put("M>>", "011000000");
+		compMap.put("M<<", "011100000");
+		compMap.put("0", "110101010");
+		compMap.put("1", "110111111");
+		compMap.put("-1", "110111010");
+		compMap.put("D", "110001100");
+		compMap.put("A", "110110000");
+		compMap.put("!D", "110001101");
+		compMap.put("!A", "110110001");
+		compMap.put("-D", "110001111");
+		compMap.put("-A", "110110011");
+		compMap.put("D+1", "110011111");
+		compMap.put("A+1", "110110111");
+		compMap.put("D-1", "110001110");
+		compMap.put("A-1", "110110010");
+		compMap.put("D+A", "110000010");
+		compMap.put("D-A", "110010011");
+		compMap.put("A-D", "110000111");
+		compMap.put("D&A", "110000000");
+		compMap.put("D|A", "110010101");
+		compMap.put("M", "111110000");
+		compMap.put("!M", "111110001");
+		compMap.put("-M", "111110011");
+		compMap.put("M+1", "111110111");
+		compMap.put("M-1", "111110010");
+		compMap.put("D+M", "111000010");
+		compMap.put("D-M", "111010011");
+		compMap.put("M-D", "111000111");
+		compMap.put("D&M", "111000000");
+		compMap.put("D|M", "111010101");
 
-	private static final String SYMBOL_DEST_AD = "AD";
-
-	private static final String SYMBOL_DEST_AMD = "AMD";
-
-	private static final String BINARY_DEST_NULL = "000";
-
-	private static final String BINARY_DEST_M = "001";
-
-	private static final String BINARY_DEST_D = "010";
-
-	private static final String BINARY_DEST_MD = "011";
-
-	private static final String BINARY_DEST_A = "100";
-
-	private static final String BINARY_DEST_AM = "101";
-
-	private static final String BINARY_DEST_AD = "110";
-
-	private static final String BINARY_DEST_AMD = "111";
-
-	//---------------------
-	// Const jump command
-	//---------------------
-
-	private static final String BINARY_JUMP_NULL = "000";
-
-	private static final String BINARY_JUMP_JGT = "001";
-
-	private static final String BINARY_JUMP_JEQ = "010";
-
-	private static final String BINARY_JUMP_JGE = "011";
-
-	private static final String BINARY_JUMP_JLT = "100";
-
-	private static final String BINARY_JUMP_JNE = "101";
-
-	private static final String BINARY_JUMP_JLE = "110";
-
-	private static final String BINARY_JUMP_JMP = "111";
+		// Set jump binary
+		jumpMap.put(null, "000");
+		jumpMap.put("JGT", "001");
+		jumpMap.put("JEQ", "010");
+		jumpMap.put("JGE", "011");
+		jumpMap.put("JLT", "100");
+		jumpMap.put("JNE", "101");
+		jumpMap.put("JLE", "110");
+		jumpMap.put("JMP", "111");
+	}
 
 
 	/**
 	 * Change binary of dest
 	 *
-	 * Change Assembly-command to dest's binary <br>
-	 * This method is called by Conpute-instruction.
+	 * Change Assembly-mnemonic to dest's binary. <br>
+	 * This method is called by Conpute-instruction. <br>
+	 * ex) 0 -> 000, M -> 001...
 	 *
-	 * @param asmCommand Assembly-command
+	 * @param mnemonic Assembly-mnemonic
 	 * @return String 3Bit dest's binary code
 	 */
-	public String dest(String asmCommand) {
-
-		// Remove but symbol
-		int index = asmCommand.indexOf("=");
-		String asmSymbol = asmCommand.substring(0, index);
-
-		if (SYMBOL_DEST_NULL.equals(asmSymbol)) {
-			// When symbol is null
-			return BINARY_DEST_NULL;
-		} else if (SYMBOL_DEST_M.equals(asmSymbol)) {
-			// When symbol is M
-			return BINARY_DEST_M;
-		} else if (SYMBOL_DEST_D.equals(asmSymbol)) {
-			// When symbol is D
-			return BINARY_DEST_D;
-		} else if (SYMBOL_DEST_MD.equals(asmSymbol)) {
-			// When symbol is MD
-			return BINARY_DEST_MD;
-		} else if (SYMBOL_DEST_A.equals(asmSymbol)) {
-			// When symbol is A
-			return BINARY_DEST_A;
-		} else if (SYMBOL_DEST_AM.equals(asmSymbol)) {
-			// When symbol is AM
-			return BINARY_DEST_AM;
-		} else if (SYMBOL_DEST_AD.equals(asmSymbol)) {
-			// When symbol is AD
-			return BINARY_DEST_AD;
-		} else if (SYMBOL_DEST_AMD.equals(asmSymbol)) {
-			// When symbol is AMD
-			return BINARY_DEST_AMD;
-		}
-		return null;
+	public String dest(String mnemonic) {
+		return destMap.get(mnemonic);
 	}
 
 
 	/**
 	 * Change binary of comp
 	 *
-	 * Change Assembly-command to comp's binary <br>
-	 * This method is called by Conpute-instruction.
+	 * Change Assembly-mnemonic to comp's binary. <br>
+	 * This method is called by Conpute-instruction. <br>
+	 * ex) 0 -> 110101010, 1 -> 110111111...
 	 *
-	 * @param asmCommand Assembly-command
+	 * @param mnemonic Assembly-mnemonic
 	 * @return String 7Bit comp's binary code
 	 */
-	public String comp(String asmCommand) {
-		return null;
+	public String comp(String mnemonic) {
+		return compMap.get(mnemonic);
 	}
 
 
 	/**
 	 * Change binary of jump
 	 *
-	 * Change Assembly-command to jump's binary <br>
-	 * This method is called by Conpute-instruction.
+	 * Change Assembly-mnemonic to jump's binary. <br>
+	 * This method is called by Conpute-instruction. <br>
+	 * ex) JGT -> 001, JEQ -> 010...
 	 *
-	 * @param asmCommand Assembly-command
+	 * @param mnemonic Assembly-mnemonic
 	 * @return String 3Bit jump's binary code
 	 */
-	public String jump(String asmCommand) {
-		return null;
+	public String jump(String mnemonic) {
+		return jumpMap.get(mnemonic);
 	}
 }
